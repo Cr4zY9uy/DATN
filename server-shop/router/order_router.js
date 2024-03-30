4cbdd7359fe4cebadcba773f4c86b306b30098fb
Cr4zY9uy <heronakamura123@gmail.com>
2024-03-30 20:30:58 +0700

    feature: create api

---
+++ server-shop/router/order_router.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import {
+    add_order,
+    edit_order,
+    detail_order,
+    all_order,
+    paginate_order,
+    delete_order_all,
+    delete_order_one,
+    delete_order_list
+} from "../controllers/order_controller.js";
+import { add_order_validator, edit_order_validator } from "../validator/order_validator.js";
+import { checkAuth } from "../middleware/check_auth.js";
+
+const router = Router();
+
+
+router.post("/order/add", add_order);
+
+router.put("/order/edit/:id", checkAuth, edit_order_validator, edit_order);
+
+router.get("/order", all_order);
+router.get("/order_paginate", paginate_order);
+router.get("/order_detail/:id", detail_order);
+
+router.delete("/order/delete/:id", checkAuth, delete_order_one);
+router.delete("/order/delete_list", checkAuth, delete_order_list);
+router.delete("/order/delete_all", checkAuth, delete_order_all);
+
+export default router;
--- server-shop/server.js
+++ server-shop/server.js
@@ -3,11 +3,13 @@ import router_auth from "./router/user_router.js";
 import router_product from "./router/product_router.js";
 import router_category from "./router/category_router.js";
 import router_order from "./router/order_router.js";
+import router_elastic from './elastic_search/elastic_search.js'
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors"
 import cookieParser from "cookie-parser";
-
+import session from "express-session";
+import './google/google-auth.js'
 const app = Express();
 dotenv.config();
 
@@ -15,13 +17,16 @@ app.use(Express.json({ limit: '10MB' }));
 app.use(Express.urlencoded({ extended: true, limit: "60mb" }));
 app.use(cors({ credentials: true, origin: ["http://localhost:3000", "http://localhost:3001"] }));
 app.use(cookieParser());
-
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}));
 app.use("/api/", router_auth);
 app.use("/api/", router_order);
 app.use("/api/", router_product);
 app.use("/api/", router_category);
-
-
+app.use("/api/", router_elastic)
 mongoose.connect(process.env.URL_DB)
     .then(() => {
         console.log("Connected to the database");
